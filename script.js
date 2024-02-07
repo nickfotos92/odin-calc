b4cecdd2b1389f587c977a08ec8a0d1ae3b16dc3
Nick Fotos <nickfotos92@gmail.com>
2024-02-06 19:47:25 -0500

    final commit

--- script.js
+++ script.js
@@ -11,27 +11,27 @@ function multiply(num1, num2) {
 }
 
 function divide(num1, num2) {
-    return num1 / num2;
+
+    if (num2 === 0) {
+        console.log("Divide by 0 error")
+        return 0;
+    } else {
+        return (num1 / num2);
+    }
 }
 
 function operate(num1, num2, type) {
 
     validOperators = ["add", "subtract", "multiply", "divide"];
 
-    if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
-        return "Please provide valid numbers";
-    } if (!validOperators.includes(type.toLowerCase())) {
-        return "Please provide a valid operator";
-    }
-
     if (type.toLowerCase() === "add") {
-        return add(num1, num2);
+        return Math.round(add(num1, num2) * 100) / 100;
     } if (type.toLowerCase() === "subtract") {
-        return subtract(num1, num2);
+        return Math.round(subtract(num1, num2) * 100) / 100;
     } if (type.toLowerCase() === "multiply") {
-        return multiply(num1, num2);
-    } if (type.toLower() === "divide") {
-        return divide(num1, num2);
+        return Math.round(multiply(num1, num2) * 100) / 100;
+    } if (type.toLowerCase() === "divide") {
+        return Math.round(divide(num1, num2) * 100) / 100;
     }
 }
 
@@ -44,6 +44,7 @@ let operator = "";
 const body = document.querySelector('body')
 const numberButtons = body.getElementsByClassName('number')
 const operatorFunctions = body.getElementsByClassName('operator')
+const clearButton = body.querySelector("#clear")
 const display = body.querySelector("#display")
 
 
@@ -57,15 +58,14 @@ Array.from(numberButtons).forEach(number => {
 Array.from(operatorFunctions).forEach(item => {
     item.addEventListener('click', function () {
 
-        if (operator) { // Is there already a pending operator?
-            savedValue = operate(parseInt(savedValue), parseInt(currentValue), operator);
-        } else { // first time
+        if (operator != '') {
+            savedValue = operate(parseFloat(savedValue), parseFloat(currentValue), operator);
+        } else {
             savedValue = currentValue;
         }
-        console.log(savedValue)
-        operator = item.id; // remember the new operator
 
-        console.log(operator)
+        operator = item.id;
+
 
         if (operator === "equal") {
             operator = ""
@@ -74,21 +74,13 @@ Array.from(operatorFunctions).forEach(item => {
         else {
             display.textContent = '';
         }
-
-
-        // display.textContent = savedValue;
-
-        // Array.from(numberButtons).forEach(number => {
-        //     number.addEventListener('click', function () {
-        //         // display.textContent = '';
-        //         newDisplayNumber = number.textContent;
-        //         display.textContent = display.textContent + newDisplayNumber;
-        //     })
-        // })
     })
 })
 
 
-// console.log(num2)
-
-// console.log(operators)
+clearButton.addEventListener('click', function () {
+    display.textContent = '';
+    operator = '';
+    currentValue = '';
+    savedValue = '';
+})
